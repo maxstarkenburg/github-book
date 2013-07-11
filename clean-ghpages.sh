506f5ef0beed3d0250d430ecddcfc553eb2a64b8
Marvin Reimer <therealmarv@gmail.com>
2013-07-11 20:30:48 +0200

    Revert "add a clean script which removes npm installed libs"
    This reverts commit 85404faa629477661b97998cb77e5e3e71c6380f.
    
    I think this clean/remove npm script is also useful for other branches
    and I also forget chmod +x for it. So revert it now for gh-pages
    and rebuild it also for other branches.

--- clean-ghpages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This script just removes library files installed by npm install so that you can do a clean
-# npm install
-# for gh-pages branch
-rm -rf node_modules/
-rm -rf scripts/libs/
